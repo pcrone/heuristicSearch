c4902e2777d75c149bd0b85d30f13ae4c38f63e1
pcrone <solidphantom@yahoo.com>
2013-11-18 14:21:26 -0600

    2nd Commit

--- src/HeuristicSearch.java
+++ src/HeuristicSearch.java
@@ -1,15 +1,74 @@
 import java.util.*;
 
 public class HeuristicSearch {
+	private Node initialState;
+	private ArrayList<Node> explored;
+	private PriorityQueue<Node> queue;
 	
-	private class Node {
+	// Constructor
+	public HeuristicSearch(String state) {
+		initialState = new Node(state);
+		explored = new ArrayList<Node>();
+		queue = new PriorityQueue<Node>();
+	}
+	
+	// Setters
+	
+	// Getters
+	
+	// Helper methods
+	public void run() {
+		
+	}
+	
+	private void runSearch() {
+		Node currentNode = initialState;
+		ArrayList<Node> currentNodeChildren;
+		queue.add(currentNode);
+		
+		while (currentNode.getHofN() != 0 || !queue.isEmpty()) {
+			currentNode = queue.remove();
+			explored.add(currentNode);
+			
+			currentNode.expand();
+			currentNodeChildren = currentNode.getChildren();
+			
+			for (int i = 0; i < currentNodeChildren.size(); i++) {
+				if (explored.contains(currentNodeChildren.get(i)) )
+					continue;
+				queue.add(currentNodeChildren.get(i));
+			}
+		}
+	}
+	
+	private Stack<Node> findPath() {
+		Stack<Node> path;
+		
+		return path;
+	}
+	
+	// Node class
+	private class Node implements Comparable<Node> {
 		private String state;
 		private ArrayList<Node> children;
 		private int hValues[];
+		private int cost;
 		
+		// Constructor
+		public Node(String state) {
+			this.state = state;
+			this.cost = 0;
+
+			calculateHofN();
+		}
+
 		// Setters
 		public void setState(String state) {
 			this.state = state;
+		}
+		
+		public void setCost(int cost) {
+			this.cost = cost;
 		}
 		
 		// Getters
@@ -19,6 +78,10 @@ public class HeuristicSearch {
 		
 		public ArrayList<Node> getChildren() {
 			return children;
+		}
+		
+		public int getCost() {
+			return cost;
 		}
 		
 		public int getHofN() {
@@ -50,15 +113,48 @@ public class HeuristicSearch {
 		}
 		
 		public void expand() {
-			// implement
+			Node child;
+			char newState[] = state.toCharArray();
+			int indexOfSpace = state.indexOf('_');
 			
+			for (int i = -3; indexOfSpace + i < state.length() && i < 4; i++) {
+				if (indexOfSpace + i < 0 || i == 0)
+					continue;
+				
+				// Swap characters
+				newState[indexOfSpace] = state.charAt(i);
+				newState[i] = '_';
+				
+				// Create the new child
+				child = new Node(newState.toString());
+				
+				// Set child path cost
+				if (Math.abs(i) > 2)
+					child.setCost(2);
+				else
+					child.setCost(1);
+				
+				// Add it to the parent	
+				children.add(child);
+			}
+		}
+
+		@Override
+		public int compareTo(Node arg0) {
+			if (this.cost < arg0.getCost())
+				return -1;
+			else if (this.cost > arg0.getCost())
+				return 1;
+			else
+				return 0;	
 		}
 		
+		public boolean equals(Node node) {
+			return state.equals(node.getState());
+		}
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		HeuristicSearch search = new HeuristicSearch("AAABBB_");
 	}
-
 }
